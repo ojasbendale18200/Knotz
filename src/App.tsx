a52297a29ef73aa6bf249899d9b0fbd47093914a
Ojas <ojasbendale18200@gmail.com>
2023-12-08 12:47:45 +0530

    setting up an routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import AuthLayout from "./_auth/AuthLayout";
+import SignIn from "./_auth/forms/SignIn";
+import SignUp from "./_auth/forms/SignUp";
+import RootLayout from "./_root/RootLayout";
+import Home from "./_root/pages/Home";
 import "./globals.css";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className='text-3xl'>App</div>
-  )
-}
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public Route */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Route>
+        {/* Private Route */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
 
-export default App
+export default App;
